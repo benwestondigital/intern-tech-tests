eb2f41e6b4900e5d5e98f5a41d9e445452412d22
Ben <benwestondigital@gmail.com>
2022-04-12 10:46:41 +0100

    complete task 1

--- react-tech-test/src/Solution/YourSolution.js
+++ react-tech-test/src/Solution/YourSolution.js
@@ -1,26 +1,58 @@
 import '../AdditionalFiles/App.css';
-import * as React from "react";
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 //This is the API url to fetch from
 const API_URL = 'https://matchesfashion.com/api/products';
 const TAX_RATE = 0.08;
 
 function YourSolution() {
-    return (
-    <div className="App">
-      <table id="products">
+  const [data, setData] = useState([]);
+  const [pageNum, setPageNum] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedData = await fetch(`${API_URL}&page=${pageNum}`);
+        const { products } = await fetchedData.json();
+        setData(products);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [pageNum]);
+
+  if (isLoading) return <p>Loading...</p>;
+  return (
+    <div className='App'>
+      <table id='products'>
         <thead>
-        <tr>
-          <th>Id</th>
-          <th>Brand</th>
-          <th>Name</th>
-          <th>Quantity Sold</th>
-          <th>Sold Price</th>
-          <th>Cost To Business</th>
-        </tr>
+          <tr>
+            <th>Id</th>
+            <th>Brand</th>
+            <th>Name</th>
+            <th>Quantity Sold</th>
+            <th>Sold Price</th>
+            <th>Cost To Business</th>
+          </tr>
         </thead>
-          <tbody>
-          </tbody>
+        <tbody>
+          {data.map(product => {
+            return (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.brand}</td>
+                <td>{product.name}</td>
+                <td>{product.quantitySold}</td>
+                <td>£{product.soldPrice}</td>
+                <td>£{product.costToBusiness}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
       <button>First Page</button>
       <button>Previous Page</button>
